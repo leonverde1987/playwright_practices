92b8680412af73760bcad98c6d34e8596ea49531
leonverde1987 <leo.aguilar.gtz@gmail.com>
2025-10-15 11:06:36 -0600

    modify test scripts

---
+++ tests/Examples/LocatorsExamples.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test"
+
+test("getByRole getByPlaceholder getByText examples", async ({ page })=> {
+    await page.goto("https://practicesoftwaretesting.com/");
+    await page.getByRole('menuitem').getByText("Sign in").click();
+    await page.getByPlaceholder('Your email').fill("customer@practicesoftwaretesting.com");
+    await page.getByPlaceholder('Your password').fill('welcome01');
+    await page.getByRole('button').getByText('Login').click();
+});
+
+test("toBeVisible toBeEnabled toContainText examples", async ({ page })=> {
+    await page.goto("https://practicesoftwaretesting.com/");
+    await page.getByRole('menuitem').getByText("Sign in").click();
+    await page.getByPlaceholder('Your email').fill("customer@practicesoftwaretesting.com");
+    await page.getByPlaceholder('Your password').fill('welcome01');
+    await page.getByRole('button').getByText('Login').click();
+    await expect(page.getByRole('button').getByText('Favorites')).toBeVisible();
+    await expect(page.getByRole('button').getByText('Profile')).toBeEnabled();
+    await expect(page.getByRole('button').getByText('Invoices')).toContainText('Invoices');
+});
